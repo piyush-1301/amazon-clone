3de57397a5810292b5b16f8ca3354c93a20be7c4
Piyush Soni <piyushsoni0069@gmail.com>
2021-01-08 13:56:51 +0530

    Initial commit  Home page

---
+++ src/Product.js
@@ -0,0 +1,27 @@
+import React from "react";
+import "./Product.css";
+
+function Product() {
+  return (
+    <div className="product">
+      <div className="product__info">
+        <p>The lean Startup</p>
+        <p className="product__price">
+          <small>$</small>
+          <strong>19.99</strong>
+        </p>
+        <div className="product__rating">
+          <p>⭐</p>
+        </div>
+      </div>
+      <img
+        src="https://images-na.ssl-images-amazon.com/images/I/81-QB7nDh4L.jpg"
+        alt=""
+      />
+
+      <button>Add to basket</button>
+    </div>
+  );
+}
+
+export default Product;
